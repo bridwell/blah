82864ff32085629e687f14d2406d9cc661accb7b
Scott Bridwell <bridwell@gmail.com>
2021-10-22 11:16:19 -0700

    'adds'

---
+++ blah/hola.py
@@ -0,0 +1,24 @@
+"""
+Some functions for testing CI and auotomated docs.
+
+"""
+
+
+def add_something(a, b):
+    """
+    Quick test function. Adds two numbers and
+    returns the result.
+
+    Parameters:
+    -----------
+    a: add-able
+        First thing
+    b: add-able
+        Second thing.
+
+    Returns:
+    --------
+    Result of adding the two.
+
+    """
+    return a + b
